30216c80b55d418dbc98ab1c6394ce5e108757a0
YounSung <jys221010@gmail.com>
2018-11-12 06:58:06 +0000

    update, delete 추가

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from todo.models import Todo
 # Create your views here.
 def index(request):
@@ -10,4 +10,44 @@ def new(request):
     return render(request,'todo/new.html')
     
 def create(request):
-    return render(request,'todo/create.html')
+    title=request.POST.get('title')
+    deadline=request.POST.get('deadline')
+    todo= Todo(title=title,deadline=deadline)
+    todo.save()
+    return redirect('/todos/')
+    
+def read(request,id):
+    todo=Todo.objects.get(id=id)
+    return render(request,'todo/read.html',{'todo':todo})
+    
+def todo_create(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        deadline = request.POST.get('deadline')
+        # todo = Todo(title=title,deadline=deadlin)
+        # todo.save()
+        Todo.objects.create(title=title,deadline=deadline)
+        return redirect('/todos/')
+    else:
+        return render(request,'todo/todo_create.html')
+        
+def update(request,id):
+    todo=Todo.objects.get(id=id)
+    if request.method=="POST":
+        #저장로직
+        todo.title=request.POST.get('title')
+        todo.deadline=request.POST.get('deadline')
+        todo.save()
+        return redirect('/todos/')
+        
+    else:
+        #폼보여주기
+        deadline=todo.deadline.strftime('%Y-%m-%d')
+        #deadline='{}-{}-{}'.format(todo.deadline.year,todo.deadline.month,todo.deadline.day)
+        return render(request,'todo/update.html',{'todo':todo,'deadline':deadline})
+
+def delete(request,id):
+    todo=Todo.objects.get(id=id)
+    todo.delete()
+    return redirect('/todos/')
+    
